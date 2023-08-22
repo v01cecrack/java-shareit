696993cede97b3b31aab66b7ad712ae50d219f30
Kostya <kostya2283221337@yandex.ru>
2023-08-22 21:22:57 +0300

    tz 16.7

--- src/main/java/ru/practicum/shareit/error/NotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error;
-
-public class NotAvailableException extends RuntimeException {
-    public NotAvailableException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/error/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error;
-
-public class ObjectNotFoundException extends RuntimeException {
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
